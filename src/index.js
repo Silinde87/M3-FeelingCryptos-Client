4cba2f2b0d40e719fb8df6d5a082d3a06872026b
Silinde87 <paurodriguezmolina@gmail.com>
2021-05-12 17:17:58 +0200

    Set style and theme

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from "./theme";
+import "./reset.css";
 import App from './App';
-
 import { AuthProvider } from './context/auth.context';
+
 
 ReactDOM.render(
   <Router>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </Router>
 , document.getElementById('root'));
---
+++ src/theme.js
@@ -0,0 +1,50 @@
+const theme = {
+    color: {
+      primary: "#FAFAFA",
+      secondary: "#E0E1DD",
+      tertiary: "#778DA9",
+      brandColor: "#004A7C",
+      letterColor1: "#0D1B2A",
+      letterColor2: "#5E6472",
+      letterColor3Sel: "#53B9EA",
+      buttonColor: "#005691",
+       
+    },
+    breakpoints: {
+      mobileMax: '420px',
+      tabletMin: '421px',
+      tabletMax: '768px',
+      desktopMin: '1025px',
+      desktopMax: '1441px',
+    },
+    typography: {
+      mulishLight: "Mulish-Light",
+      mulishMedium: "Mulish-Medium",
+      mulishRegular: "Mulish-Regular",
+      mulishSemiBold: "Mulish-SemiBold"
+    },
+    typographySizes: {
+      s: {
+        size: '12px',
+        line: '15px',
+      },
+      m: {
+        size: '16px',
+        line: '19px',
+      },
+      l: {
+        size: '32px',
+        line: '35px',
+      },
+      xl: {
+        size: '96px',
+        line: '106px',
+      },
+    },
+    spacing: {
+      small: '16px',
+      medium: '32px',
+      large: '40px',
+    },
+  };
+  export default theme;

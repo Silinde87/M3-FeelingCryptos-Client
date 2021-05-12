e6e09884290012a52da84a6859e566d6ba9ee95f
Silinde87 <paurodriguezmolina@gmail.com>
2021-05-12 23:20:20 +0200

    Login form with validations

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react";
+import Text from "../text";
 
 export default function Form(props) {
-    const {email, password} = props.state;
-    console.log(email, password);
-    return (
-        <form onSubmit={props.handleFormSubmit}>
-        {
-            props.fields.map(el => {
-                console.log(el);
-                return(
-                    <div>
-                        <label>{el}</label>
-                        <input type={el} name={el} value={email} ></input>
-                    </div>
-                );
-            })
-        }
-            
-        </form>
-    )
+	const { fields, errors } = props.state;
+	const keys = Object.keys(fields);
+	const values = Object.values(fields);
+	return (
+		<form id="form" onSubmit={props.handleFormSubmit}>
+			{keys.map((el, i) => {
+				return (
+					<div className="form-group" key={el}>
+						<label className="form-label" htmlFor={`${el}-input`}>
+                            <Text size="m">Introduce your {el}</Text>
+						</label>
+						<input
+							className="form-input"
+							id={`${el}-input`}
+							aria-describedby={`${el}Help`}
+							type={el}
+							name={el}
+							value={values[i]}
+							onChange={props.handleChange}
+						></input>
+						{errors[el] && <Text >{errors[el]}</Text>}
+					</div>
+				);
+			})}
+			<input id="form-btn" type="submit" value="Login" />
+		</form>
+	);
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,44 +1,79 @@
 import React, { Component } from "react";
 import Form from "../../components/Form/Form";
-import { withAuth } from '../../context/auth.context';
+import { withAuth } from "../../context/auth.context";
+import Text from "../../components/text";
+const EMAIL_PATTERN =
+	/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+//Fields validators
+const validators = {
+	email: (value) => {
+		let message;
+		if (!value) {
+			message = "Email is required";
+		} else if (!EMAIL_PATTERN.test(value)) {
+			message = "Invalid email";
+		}
+		return message;
+	},
+	password: (value) => {
+		let message;
+		if (!value) {
+			message = "Password is required";
+		} else if (value.length < 6) {
+			message = "Invalid password";
+		}
+		return message;
+	},
+};
 
 class Login extends Component {
-  state = { email: "", password: "" };
+	state = {
+		fields: {
+			email: "",
+			password: "",
+		},
+		errors: {
+			email: null,
+			password: null,
+		},
+	};
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    const { email, password } = this.state;
-    // Call function coming from AuthProvider ( via withAuth )
-    this.props.login({ email, password });
-  };
+	handleFormSubmit = (event) => {
+		event.preventDefault();
+		const { email, password } = this.state.fields;
+		// Call function coming from AuthProvider ( via withAuth )
+		this.props.login({ email, password });
+	};
 
-  handleChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
+	handleChange = (event) => {
+		const { name, value } = event.target;
+		this.setState({
+			fields: {
+				...this.state.fields,
+				[name]: value,
+			},
+			errors: {
+				...this.state.errors,
+				[name]: validators[name](value),
+			},
+		});
+	};
 
-  render() {
-    const { email, password } = this.state;
-    const fields = ["email", "password"];
-
-    return (
-      <div>
-        <h1>Login</h1>
-        <Form onSubmit={this.handleFormSubmit} onChange={this.handleChange} fields={fields} state={this.state}/>
-
-        <form onSubmit={this.handleFormSubmit}>
-          
-          <label>Email:</label>
-          <input type="email" name="email" value={email} onChange={this.handleChange}/>
-
-          <label>Password:</label>
-          <input type="password" name="password" value={password} onChange={this.handleChange} />
-
-          <input type="submit" value="Login" />
-        </form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<section id="form-container">
+				<Text as="h2" size="l" weight="mulishMedium">
+					Login
+				</Text>
+				<Form
+					handleFormSubmit={(e) => this.handleFormSubmit(e)}
+					handleChange={(e) => this.handleChange(e)}
+					state={this.state}
+				/>
+			</section>
+		);
+	}
 }
 
 export default withAuth(Login);

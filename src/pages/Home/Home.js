2dd3893bc06c487d01f9070d590cc9bae62c6ccf
Taisia Makhmutova <taia.makh@gmail.com>
2021-05-13 15:24:37 +0200

    add component websocket

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import WebsocketBinance from '../../components/WebsocketBinance/WebsocketBinance'
+
 
 function Home() {
   return (
     <div> 
       <h1>Home Page</h1>
+      <div> <WebsocketBinance /> </div>
     </div>
   )
 }

24438c022e494f4f669c11894b887643152fb443
Silinde87 <paurodriguezmolina@gmail.com>
2021-05-20 15:53:59 +0200

    Fix style marketlist

--- src/components/ProfileFeed/ProfileFeed.js
+++ src/components/ProfileFeed/ProfileFeed.js
@@ -3,22 +3,31 @@ import { withAuth } from "../../context/auth.context";
 import SCProfileFeed from "./ProfileFeed.styled";
 import TweetFeed from "../TweetFeed/TweetFeed";
 import { Link } from "react-router-dom";
-import Text from "../text"
+import Text from "../text";
 
 class ProfileFeed extends Component {
 	render() {
 		//Getting favorites cryptos tweets from user.
-        const { favorites_cryptos } = this.props.user;
+		const { favorites_cryptos } = this.props.user;
 		return (
-			<>	
-			<SCProfileFeed id="profile-feed-container">
-			{ favorites_cryptos.length ? 		
-				<TweetFeed favorites_cryptos={favorites_cryptos}/>
-			:
-			<Link className="link-add" to="/"><h1> <Text  weight="mulishMedium" size="m" color="letterColor3Sel"> ADD A FAVORITE MARKET </Text></h1></Link> 
-			}
-			</SCProfileFeed>
-		
+			<>
+				<SCProfileFeed id="profile-feed-container">
+					{favorites_cryptos.length ? (
+						<TweetFeed favorites_cryptos={favorites_cryptos} />
+					) : (
+						<section>
+							<Link className="link-add" to="/">
+								<h1>
+									{" "}
+									<Text weight="mulishMedium" size="m" color="letterColor3Sel">
+										{" "}
+										ADD A FAVORITE MARKET{" "}
+									</Text>
+								</h1>
+							</Link>
+						</section>
+					)}
+				</SCProfileFeed>
 			</>
 		);
 	}
